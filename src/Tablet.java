689820c7ca89b714df3a41821c8705b06f398d19
BT_1E10_03 <sample@bths.edu>
2022-03-16 10:45:06 -0400

    commmit

--- src/Tablet.java
+++ src/Tablet.java
@@ -1,13 +1,10 @@
-public class Tablet
+public class Tablet extends Computer
 {
-    private int screenSize; // in inches
-    private int memory; // in gigabytes (GB)
     private boolean stylusIncluded;
 
     public Tablet(int screenSize, int memory, boolean stylusIncluded)
     {
-        this.screenSize = screenSize;
-        this.memory = memory;
+        super(screenSize, memory);
         this.stylusIncluded = stylusIncluded;
     }
 
